d0f036c7e0b9707e4401ada6395e5e92cdde4650
therealdazl <louis.theriault@outlook.fr>
2024-07-06 10:01:43 -0400

    Major modifications. I have added a class, and I have optimized my code further.

--- Cipher/Cipher.cs
+++ Cipher/Cipher.cs
@@ -9,83 +9,25 @@ namespace Cipher
 {
     internal class Cipher
     {
-        static private string CharacterLibrary = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .";
-
-        static public string ValidateLibrary(string library)
-        {
-            if (library != "")
-            {
-                for (int i = 0; i < library.Length - 1; i++)
-                {
-                    if (library.Substring(i + 1).Contains(library[i]))
-                    {
-                        throw new ArgumentException("The library must be made from unique characters.");
-                    }
-                }
-
-                CharacterLibrary = library;
-            }
-
-            return CharacterLibrary;
-        }
-
-        static public string ValidateInput(string input, string type)
-        {
-            int inputLength = input.Length;
-
-            if (type == "Message" && (inputLength == 0 || inputLength > CharacterLibrary.Length))
-            {
-                throw new ArgumentException($"{type} must be at least one character long, and shorter or equal to the length of the library.");
-            }
-
-            else if (type != "Message" && inputLength > CharacterLibrary.Length)
-            {
-                throw new ArgumentException($"{type} must be shorter or equal to the length of the library.");
-            }
-
-            if (!input.All(CharacterLibrary.Contains))
-            {
-                throw new ArgumentException($"Invalid characters in {type}. All characters from {type} must also be present in the library.");
-            }
-
-            if (inputLength < CharacterLibrary.Length)
-            {
-                for (int i = inputLength; i < CharacterLibrary.Length; i++)
-                {
-                    if (type != "Message" && inputLength != 0)
-                    {
-                        input += input[i % inputLength];
-                    }
-
-                    else
-                    {
-                        input += CharacterLibrary[RandomNumberGenerator.GetInt32(CharacterLibrary.Length)];
-                    }
-                }
-            }
-
-            return input;
-        }
-
-        static private string SubstituteCharacters(string message, string key, bool encode)
+        static private string SubstituteCharacters(string library, string message, string key, bool encode)
         {
             var result = new char[message.Length];
 
             for (int i = 0; i < message.Length; i++)
             {
-                int msgIndex = CharacterLibrary.IndexOf(message[i]);
-                int keyIndex = CharacterLibrary.IndexOf(key[i]);
+                int msgIndex = library.IndexOf(message[i]);
+                int keyIndex = library.IndexOf(key[i]);
                 int sign = encode ? 1 : -1;
 
-                int newIndex = (msgIndex + sign * keyIndex + CharacterLibrary.Length) % CharacterLibrary.Length;
+                int newIndex = (msgIndex + sign * keyIndex + library.Length) % library.Length;
 
-                result[i] = CharacterLibrary[newIndex];
+                result[i] = library[newIndex];
             }
 
             return new string(result);
         }
 
-        static private string TransposeCharacters(string message, string key, bool encode)
+        static private string TransposeCharacters(string library, string message, string key, bool encode)
         {
             var charArray = message.ToCharArray();
 
@@ -95,7 +37,7 @@ namespace Cipher
 
             for (int i = start; i != end; i += step)
             {
-                int swapIndex = (i + CharacterLibrary.IndexOf(key[i])) % CharacterLibrary.Length;
+                int swapIndex = (i + library.IndexOf(key[i])) % library.Length;
 
                 (charArray[i], charArray[swapIndex]) = (charArray[swapIndex], charArray[i]);
             }
@@ -103,11 +45,11 @@ namespace Cipher
             return new string(charArray);
         }
 
-        static public string EncodeDecode(string message, string substitutionKey, string transpositionKey, bool encode)
+        static public string EncodeDecode(string library, string message, string substitutionKey, string transpositionKey, bool encode)
         {
             return encode
-                  ? TransposeCharacters(SubstituteCharacters(message, substitutionKey, encode), transpositionKey, encode)
-                  : SubstituteCharacters(TransposeCharacters(message, transpositionKey, encode), substitutionKey, encode);
+                   ? TransposeCharacters(library, SubstituteCharacters(library, message, substitutionKey, encode), transpositionKey, encode)
+                   : SubstituteCharacters(library, TransposeCharacters(library, message, transpositionKey, encode), substitutionKey, encode);
         }
     }
 }
---
+++ Cipher/MessageObj.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher
+{
+    internal class MessageObj
+    {
+        private string CharacterLibrary { get; set; }
+        private string Message { get; set; }
+        private string SubstitutionKey { get; set; }
+        private string TranspositionKey { get; set; }
+        private bool Encode { get; set; }
+
+        public MessageObj()
+        {
+            while (!SetLibrary()) ;
+            Console.WriteLine($"Your library contains {CharacterLibrary.Length} characters. Here is your library:\n{CharacterLibrary}\n");
+
+            while (!SetInput("Message"));
+            Console.WriteLine($"Here is your message:\n{Message}\n");
+
+            while (!SetInput("Substitution Key"));
+            Console.WriteLine($"Here is your substitution key:\n{SubstitutionKey}\n");
+
+            while (!SetInput("Transposition Key"));
+            Console.WriteLine($"Here is your transposition key:\n{TranspositionKey}\n");
+
+            while (!SetEncodeOrDecode());
+            string value = Encode ? "encode" : "decode";
+            Console.WriteLine($"You have chosen to {value} your message.\n");
+        }
+
+
+        private bool SetLibrary()
+        {
+            Console.WriteLine("Enter your character library as a string, then press enter:");
+            string library = Console.ReadLine();
+
+            try
+            {
+                if (library != "")
+                {
+                    for (int i = 0; i < library.Length - 1; i++)
+                    {
+                        if (library.Substring(i + 1).Contains(library[i]))
+                        {
+                            throw new ArgumentException("The library must be made from unique characters.");
+                        }
+                    }
+
+                    CharacterLibrary = library;
+                }
+
+                else
+                {
+                    CharacterLibrary = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .";
+                }
+
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
+
+        private bool SetInput(string type)
+        {
+            string input;
+
+            if (type == "Message")
+            {
+                Console.WriteLine("Enter your message, then press enter:");
+                input = Console.ReadLine();
+            }
+
+            else if (type == "Substitution Key")
+            {
+                Console.WriteLine("Enter your substitution key, then press enter:");
+                input = Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Enter your transposition key, then press enter:");
+                input = Console.ReadLine();
+            }
+
+            try
+            {
+                int inputLength = input.Length;
+
+                if (type == "Message" && (inputLength == 0 || inputLength > CharacterLibrary.Length))
+                {
+                    throw new ArgumentException($"{type} must be at least one character long, and shorter or equal to the length of the library.");
+                }
+
+                else if (type != "Message" && inputLength > CharacterLibrary.Length)
+                {
+                    throw new ArgumentException($"{type} must be shorter or equal to the length of the library.");
+                }
+
+                if (!input.All(CharacterLibrary.Contains))
+                {
+                    throw new ArgumentException($"Invalid characters in {type}. All characters from {type} must also be present in the library.");
+                }
+
+                if (inputLength < CharacterLibrary.Length)
+                {
+                    for (int i = inputLength; i < CharacterLibrary.Length; i++)
+                    {
+                        if (type != "Message" && inputLength != 0)
+                        {
+                            input += input[i % inputLength];
+                        }
+
+                        else
+                        {
+                            input += CharacterLibrary[RandomNumberGenerator.GetInt32(CharacterLibrary.Length)];
+                        }
+                    }
+                }
+
+                if (type == "Message")
+                {
+                    Message = input;
+                }
+
+                else if (type == "Substitution Key")
+                {
+                    SubstitutionKey = input;
+                }
+
+                else
+                {
+                    TranspositionKey = input;
+                }
+
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
+
+        private bool SetEncodeOrDecode()
+        {
+            Console.WriteLine("Enter your choice, then press enter. The value 1 is for encoding, and the value 2 is for decoding:");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Encode = true;
+
+                return true;
+            }
+
+            else if (choice == "2")
+            {
+                Encode = false;
+
+                return true;
+            }
+
+            Console.WriteLine("Your choice must be 1 or 2. The value 1 is for encoding, and the value 2 is for decoding.");
+
+            return false;
+        }
+
+        public void GetEncodedOrDecodedMessage()
+        {
+            Console.WriteLine("Here is the processed message:");
+            string encodedMessage = Cipher.EncodeDecode(CharacterLibrary, Message, SubstitutionKey, TranspositionKey, Encode);
+            Console.WriteLine(encodedMessage);
+
+            Console.WriteLine("Here is the original message:");
+            string decodedMessage = Cipher.EncodeDecode(CharacterLibrary, encodedMessage, SubstitutionKey, TranspositionKey, !Encode);
+            Console.WriteLine(decodedMessage);
+        }
+    }
+}

73f6fa2804e6e4427d88b9cf66f2d3cf3de99423
therealdazl <louis.theriault@outlook.fr>
2024-07-18 14:42:02 -0400

    I renamed some constants.

--- Cipher/Program.cs
+++ Cipher/Program.cs
@@ -5,16 +5,16 @@
         static void Main(string[] args)
         {
             bool quit = false;
-            const string INITIALMESSAGE = "*** Polyalphabetic block cipher ***\n\nMy cipher uses polyalphabetic substitution and transposition to encode and decode messages. " +
+            const string INITIAL_MESSAGE = "*** Polyalphabetic block cipher ***\n\nMy cipher uses polyalphabetic substitution and transposition to encode and decode messages. " +
                 "Now, the character library comes with a default value, but the library is in fact completely modulable. The only restrictions to my cipher are the following:\n\n" +
                 "1) The library must be made from unique characters, that is, a specific character cannot appear twice;\n" +
                 "2) The message and the keys cannot contain characters that are not in the library;\n" +
                 "3) The individual message's and keys' character count cannot be longer than the library's character count;\n" +
                 "4) The message cannot be empty, however the keys can be empty. My program will use padding if the message or the keys are not as long as the library. " +
                 "That means the program will add characters at the end of the input, so as to make the input become the same length as the library.\n";
-            const string FINALMESSAGE = "\nDo you want to quit the program, or do you want to continue? Type \"q\" to quit, or \"c\" to continue, then press Enter.";
+            const string FINAL_MESSAGE = "\nDo you want to quit the program, or do you want to continue? Type \"q\" to quit, or \"c\" to continue, then press Enter.";
 
-            Console.WriteLine(INITIALMESSAGE); 
+            Console.WriteLine(INITIAL_MESSAGE); 
 
             while (!quit)
             {
@@ -30,7 +30,7 @@
                     Console.WriteLine("Error: " + ex.Message);
                 }
 
-                Console.WriteLine(FINALMESSAGE);
+                Console.WriteLine(FINAL_MESSAGE);
                 quit = (Console.ReadLine() == "q" ? true : false);
                 Console.Write("\n");
             }
